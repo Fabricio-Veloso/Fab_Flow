1ab36e4f14a57b837bf4e237138e848d12be6a8f
Fabricio-Veloso <fabraccho@gmail.com>
2025-01-16 11:03:02 -0300

    vertical spacing on imports

--- utils/checkcoreassetshealth.ts
+++ utils/checkcoreassetshealth.ts
@@ -1,16 +1,13 @@
 
-import { checkAssetsConflicts} from './checkassetsconflicts'
-
-import { Vault} from 'obsidian'
-import {checkCoreAssetsPath} from './checkcoreassetspath'
-import {Asset} from '../interfaces/Asset'
+import {checkAssetsConflicts} from './checkassetsconflicts'
+import {Vault}                from 'obsidian'
+import {checkCoreAssetsPath}   from './checkcoreassetspath'
+import {Asset}                 from '../interfaces/Asset'
 
 export async function checkCoreAssetsHealth(vault: Vault ,coreAssetsDictionary: { [key: string]: Asset},coreSetupReq:{ [key:string]:boolean}){
 
 	await checkCoreAssetsPath(vault,coreAssetsDictionary,'conflicting','Dont exist'); 
 	console.log(coreAssetsDictionary);
 	await checkAssetsConflicts(coreAssetsDictionary,coreSetupReq.anyConflictOnPath);
-	
-
 
 }

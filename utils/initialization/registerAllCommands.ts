b3152df70d6c03b2a91f065f3dfe4166a9d2f4c6
Fabricio-Veloso <fabraccho@gmail.com>
2025-01-24 12:35:01 -0300

    Added register Active Commands Panel command

--- utils/initialization/registerAllCommands.ts
+++ utils/initialization/registerAllCommands.ts
@@ -2,10 +2,12 @@ import {Plugin} from "obsidian";
 import {registerCreateNewTask} from "../commands/createNewTask/registerCreateNewTask"
 import {registerCreateNewProject} from "../commands/createNewProject/registerCreateNewProject"
 import {registerCreateNewEvent} from "../commands/createNewEvent/registerCreateNewEvent"
+import { registerActiveCommandsPanel } from "../commands/activateCommandsPanel/registerActiveCommandsPanel";
 
 export function registerAllCommands(plugin: Plugin){
 	registerCreateNewTask(plugin);	
 	registerCreateNewProject(plugin);
 	registerCreateNewEvent(plugin);
+	registerActiveCommandsPanel(plugin);	
 }
 

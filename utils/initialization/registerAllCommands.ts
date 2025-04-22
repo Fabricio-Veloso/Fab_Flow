f41aae626dba604c695c87808b7376930db62ea7
Fabricio-veloso <fabraccho@gmail.com>
2025-04-22 19:46:31 -0300

    Changing the name of the command for the new name.

--- utils/initialization/registerAllCommands.ts
+++ utils/initialization/registerAllCommands.ts
@@ -1,13 +1,12 @@
 import {Plugin} from "obsidian";
 import {registerCreateNewTask} from "../commands/createNewTask/registerCreateNewTask"
-import {registerCreateNewProject} from "../commands/createNewProject/registerCreateNewProject"
 import {registerCreateNewEvent} from "../commands/createNewEvent/registerCreateNewEvent"
 import { registerActiveCommandsPanel } from "../commands/activateCommandsPanel/registerActiveCommandsPanel";
-
+import {registerCreateProject} from "../commands/CreateProject/registerCreateProject"
 export function registerAllCommands(plugin: Plugin){
 	registerCreateNewTask(plugin);	
-	registerCreateNewProject(plugin);
 	registerCreateNewEvent(plugin);
 	registerActiveCommandsPanel(plugin);	
+	registerCreateProject(plugin);
 }
 

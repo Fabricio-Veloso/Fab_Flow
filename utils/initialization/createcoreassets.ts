690a7f2ce46afbc425bb98891d2e0f83c6a5bab1
Fabricio-veloso <fabraccho@gmail.com>
2025-04-22 19:50:32 -0300

    This commit will be squashed in the future.

--- utils/initialization/createcoreassets.ts
+++ utils/initialization/createcoreassets.ts
@@ -1,22 +1,19 @@
-import {Vault} from 'obsidian';
-import {Asset} from '../interfaces/Asset'
-
-export async function createCoreAssets(vault:Vault,generalStatus: { [key: string]: Asset },isSetupDone:boolean){
+export async function createCoreAssets(vault: Vault, generalStatus: { [key: string]: Asset }, isSetupDone: boolean) {
 	for (const [key, { path, status }] of Object.entries(generalStatus)) {
-		if (path.endsWith('.md')){
-			if(isSetupDone == true){
+		const isMarkdown = path.endsWith('.md');
+		const exists = await vault.adapter.exists(path);
+
+		if (isMarkdown) {
+			if ((isSetupDone && !exists) || (!isSetupDone && status === 'missing')) {
 				await vault.adapter.write(path, '# New Template\n\nThis is a template.');
 				console.log(`"${path}" created as a Markdown file`);
-			}else{
-				if(status == 'missing'){
-					await vault.adapter.write(path, '# New Template\n\nThis is a template.');
-					console.log(`"${path}" created as a Markdown file`);
-				}
 			}
-		}else{
-			await vault.adapter.mkdir(path);
-			console.log(`"${path}" created as a directory`);
+		} else {
+			if (!exists) {
+				await vault.adapter.mkdir(path);
+				console.log(`"${path}" created as a directory`);
+			}
 		}
-		
 	}
 }
+

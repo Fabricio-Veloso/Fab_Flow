2e67c93e8683aeeda9ce136c35f3a49c5407be68
Fabricio-veloso <fabraccho@gmail.com>
2025-05-02 16:33:37 -0300

    Simple correction on import path.

--- utils/commands/createNewEvent/registerCreateNewEvent.ts
+++ utils/commands/createNewEvent/registerCreateNewEvent.ts
@@ -1,6 +1,6 @@
 import { Plugin } from 'obsidian';
 import {createNewEventCb} from './createNewEventcb';
-import {PLUGIN_ID} from "../../consts.ts";
+import {PLUGIN_ID} from "../../consts";
 
 export function registerCreateNewEvent(plugin: Plugin) {
     plugin.addCommand({

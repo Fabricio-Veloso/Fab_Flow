b9184597c0b9c439333dde8ee5828d07de39ce32
Fabricio-veloso <fabraccho@gmail.com>
2025-05-02 11:53:45 -0300

    Deleting legacy function.

--- utils/initialization/scrapecardsbypriority.ts
+++
@@ -1,7 +0,0 @@
-export function scrapeCardByPriority(){
-//On start
-	//    Goes to the projects folder
-	//    Scrapes each card from each project Kanban Board
-	//    Creates a list orded by the priority metadata    
-	//    returns it
-}

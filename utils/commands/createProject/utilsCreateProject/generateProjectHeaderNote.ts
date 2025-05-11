42758c2e41992bf16030bb4e2d7fdb016bdb29d0
Fabricio-veloso <fabraccho@gmail.com>
2025-05-11 12:08:26 -0300

    String for the project Header fixed.

--- utils/commands/createProject/utilsCreateProject/generateProjectHeaderNote.ts
+++ utils/commands/createProject/utilsCreateProject/generateProjectHeaderNote.ts
@@ -1,4 +1,4 @@
-import { App, normalizePath } from "obsidian";
+import { App, normalizePath, Notice } from "obsidian";
 import { ColumnData, ActivityData} from "../../../../components/modals/projectBoardBuilderModal";
 import {getBaseProjectPath,} from "../../../core/resolvePathsFunctions";
 import {createFileIfNotExists} from "../../../core/createFileIfNotExists";
@@ -8,21 +8,7 @@ export async function generateProjectHeaderNote(app: App, project: ProjectInfo) 
 	const basePath = getBaseProjectPath(project.scope, project.name);
 	const headerPath = normalizePath(`${basePath}/${project.name}_h.md`);
 
-	const headerContent = `## Status
-	${project.status ?? ''}
-
-	## Files
-	${project.files ?? ''}
-
-	## Context
-	${project.context ?? ''}
-
-	## Objectives
-	${project.objectives ?? ''}
-
-	## Roadmap
-	${project.roadmap ?? ''}`;
-
+	const headerContent = `## Status\n${project.status ?? ''}\n\n## Files\n${project.files ?? ''}\n\n## Context\n${project.context ?? ''}\n\n## Objectives\n${project.objectives ?? ''}\n\n## Roadmap\n${project.roadmap ?? ''}`;
 	await ensureFolderExists(app, basePath);
 	await createFileIfNotExists(app, headerPath, headerContent);
 }

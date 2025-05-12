e7f04f215c544429cbd5cecaaac45359685f255a
Fabricio-veloso <fabraccho@gmail.com>
2025-05-11 22:12:39 -0300

    Fixing function name.

--- utils/commands/logProgressComment/registerlogProgressComment.ts
+++ utils/commands/logProgressComment/registerlogProgressComment.ts
@@ -1,12 +1,12 @@
 import {Plugin} from "obsidian";
-import {logProgressComment} from './logProgressCommentCb'
+import {logProgressCommentCb} from './logProgressCommentCb'
 import {PLUGIN_ID} from '../../consts'
 
 export function registerlogProgressComment(plugin: Plugin) {
 	plugin.addCommand({
 		id: `${PLUGIN_ID}:logProgressComment`,
 		name: "logProgressComment",
-		callback: () => logProgressComment(plugin)
+		callback: () => logProgressCommentCb(plugin)
 	});
 }
 

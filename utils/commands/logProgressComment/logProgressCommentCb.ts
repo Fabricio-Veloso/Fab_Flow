f420b2d5338f65e54b73377db3e012f36fd0bb20
Fabricio-veloso <fabraccho@gmail.com>
2025-05-11 22:12:56 -0300

    Adding some pseudocode.

---
+++ utils/commands/logProgressComment/logProgressCommentCb.ts
@@ -0,0 +1,15 @@
+import {markdownComment } from "../../../components/modals/markdownComment.ts";
+import { App } from "obsidian";
+
+logProgressCommentCb() {
+
+	// Get all the scopes names, project names and activities...
+	//	Get the current scope, project and activity.
+	//	Create a modal instance
+	//	Fill the modal with the current scope, project and activity.
+	//	Show the modal to the user.
+	//	Retrive data.
+	//	use the chosen activitie status or _h project note to add the log.
+	//	Send feedback to user.
+
+}
